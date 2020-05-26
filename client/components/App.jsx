6836fe2fe04af69174e9da8574720c0683153e9f
Ugur Mehmetoglu <ugurmehmetoglu@Ugurs-MacBook-Air.local>
2020-05-26 11:43:56 -0700

    setup index.jsx

---
+++ client/components/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+import axios from 'axios'
+
+function App (){
+
+    
+
+
+    return(
+        <div>
+            <h1>Merhaba</h1>
+        </div>
+    )
+}
+
+
+export default App;
