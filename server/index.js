c1c5325a749278ba8107d25c54fbb6cd6d9831d2
Ugur Mehmetoglu <ugurmehmetoglu@Ugurs-MacBook-Air.local>
2020-05-27 20:52:59 -0700

    remove the person model variable

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
-const Person = require("../database/index.js").Person
 const path=require('path')
 const bodyParser = require('body-parser')
 const port = 3000
 const{Sequelize}=require('sequelize')
 const sequelize = require('../database/index.js').sequelize
+const Product = require("../database/index.js").Product
 
 
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -13,27 +13,30 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, '../static')))
 
 
-app.get('/people', (req, res) => 
-Person.findAll().then((result)=>res.send(result))
+app.get('/products', (req, res) => 
+    Product.findAll().then((result)=>res.send(result))
 );
 
-app.post("/people", (req,res) => {
+app.post("/products", (req,res) => {
     
-    Person.create({
-        name:req.body.name,
-        lastName:req.body.lastName
+    Product.create({
+       imgURL:req.body.imgURL,
+       type:req.body.type,
+       name:req.body.name,
+       price:req.body.price
+
     })
     .then(result => res.send(result))
     .catch(err =>res.send(err))
+
     
 }
 
 );
 
-app.put('/people', function(req,res){
-    Person.update({
-        name:req.body.name,
-        lastName:req.body.lastName
+app.put('/products', function(req,res){
+    Product.update({
+       price:req.body.price
     },{
         where:{
             id:req.body.id
@@ -42,10 +45,11 @@ app.put('/people', function(req,res){
     .catch(err => console.log(err))
 })
 
-app.delete('/people', function(req,res){
-    Person.destroy({
+app.delete('/products', function(req,res){
+    
+    Product.destroy({
         where:{
-            id:req.body.id
+            id:req.query.id
         }
     }).then(result => res.send("deleted"))
     .catch(err => res.send(err))
